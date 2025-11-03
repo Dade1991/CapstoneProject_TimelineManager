1816cd67d7d994ea9aae8a0824824e6ab7ebd213
Dade1991 <dade91@msn.com>
2025-11-03 14:19:16 +0100

    Initialized PJT folder

---
+++ src/main/java/davidebraghi/CapstoneProject_TimelineManager/CapstoneProjectTimelineManagerApplication.java
@@ -0,0 +1,13 @@
+package davidebraghi.CapstoneProject_TimelineManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectTimelineManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectTimelineManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davidebraghi/CapstoneProject_TimelineManager/CapstoneProjectTimelineManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package davidebraghi.CapstoneProject_TimelineManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectTimelineManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
