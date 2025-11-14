5d86e9a6037befa71809057b9442889353974135
Dade1991 <dade91@msn.com>
2025-11-14 16:39:03 +0100

    Minor changes due Front-end development necessities

--- src/main/java/davidebraghi/CapstoneProject_TimelineManager/Payload_DTO/Task_DTO_RequestsAndResponses/TaskUpdateRequest.java
+++ src/main/java/davidebraghi/CapstoneProject_TimelineManager/Payload_DTO/Task_DTO_RequestsAndResponses/TaskUpdateRequest.java
@@ -2,18 +2,20 @@ package davidebraghi.CapstoneProject_TimelineManager.Payload_DTO.Task_DTO_Reques
 
 import davidebraghi.CapstoneProject_TimelineManager.enums.TaskPriorityENUM;
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 public record TaskUpdateRequest(
-        @Size(min = 3, max = 100, message = "Task title should have 3 to 100 characters.")
+        @Size(min = 3, max = 100, message = "Task title Update Ta 3 to 100 characters.")
         String taskTitle,
 
         @Size(max = 500, message = "Description should have max 500 characters.")
         String taskDescription,
 
+        @NotNull(message = "Priority is mandatory.")
         TaskPriorityENUM taskPriority,
 
         Long statusId,
