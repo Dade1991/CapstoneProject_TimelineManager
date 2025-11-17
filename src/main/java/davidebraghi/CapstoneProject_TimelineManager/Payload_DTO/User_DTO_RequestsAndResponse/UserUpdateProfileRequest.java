074b44d87e01d5de815226caa696edbabaeb72e3
Dade1991 <dade91@msn.com>
2025-11-17 20:20:11 +0100

    Minor changes

--- src/main/java/davidebraghi/CapstoneProject_TimelineManager/Payload_DTO/User_DTO_RequestsAndResponse/UserUpdateProfileRequest.java
+++ src/main/java/davidebraghi/CapstoneProject_TimelineManager/Payload_DTO/User_DTO_RequestsAndResponse/UserUpdateProfileRequest.java
@@ -4,13 +4,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
 public record UserUpdateProfileRequest(
-        @Size(min = 2, max = 50, message = "Name should have 2 to 50 characters.")
+        @Size(message = "Name should have 2 to 50 characters.")
         String name,
 
-        @Size(min = 2, max = 50, message = "Surname should have 2 to 50 characters.")
+        @Size(message = "Surname should have 2 to 50 characters.")
         String surname,
 
-        @Size(min = 3, max = 30, message = "Nickname should have 3 to 30 characters.")
+        @Size(message = "Nickname should have 3 to 30 characters.")
         String nickname,
 
         @Email(message = "The email is not in the correct format.")
