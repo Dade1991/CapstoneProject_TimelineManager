e5250f1318441e2be7c1117618adae996d2cf007
Dade1991 <dade91@msn.com>
2025-11-17 14:50:13 +0100

    Minor changes for Front-end allign reasons

--- src/main/java/davidebraghi/CapstoneProject_TimelineManager/configuration/WebConfig.java
+++ src/main/java/davidebraghi/CapstoneProject_TimelineManager/configuration/WebConfig.java
@@ -1,11 +1,25 @@
 package davidebraghi.CapstoneProject_TimelineManager.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableSpringDataWebSupport(
         pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
 )
 public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173") // porta front end usata da npm run dev
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
